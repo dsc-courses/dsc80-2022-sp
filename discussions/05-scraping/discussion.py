ab3fc28883b095d1e49184a9bc1d438c34ec76e9
Murali Mohana Krishna Dandu <murali.munna94@gmail.com>
2022-05-04 11:57:29 -0700

    added discussion 05

---
+++ discussions/05-scraping/discussion.py
@@ -0,0 +1,48 @@
+# discussion.py
+
+
+import os
+import numpy as np
+import pandas as pd
+import requests
+import time
+from bs4 import BeautifulSoup
+import re
+
+
+# ---------------------------------------------------------------------
+# QUESTION 1
+# ---------------------------------------------------------------------
+
+
+def get_website_urls():
+    """
+    Get all the website URLs
+
+    :Example:
+    >>> urls = get_website_urls()
+    >>> len(urls)
+    50
+    >>> 'catalogue' in urls[0]
+    True
+    """
+    ...
+
+
+# ---------------------------------------------------------------------
+# QUESTION 2
+# ---------------------------------------------------------------------
+
+
+def book_categories():
+    """
+    Get all the book categories and return them as a list
+
+    :Example:
+    >>> categories = book_categories()
+    >>> len(categories)
+    50
+    >>> 'Classics' in categories
+    True
+    """
+    ...
